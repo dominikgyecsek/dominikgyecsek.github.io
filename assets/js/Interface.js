d9694cadcc6358ebb3c3f6e48467b6e5569e641e
Dominik Gyécsek <gyecsek.dominik@gmail.com>
2016-10-09 17:27:56 +0100

    implemented msg send and subscribe

--- assets/js/Interface.js
+++ assets/js/Interface.js
@@ -5,6 +5,24 @@ $(document).ready(function() {
 	$(".input").focus(function() { interface.inputEnter( $(this) ) })
 	$(".input").blur(function() { interface.inputLeave( $(this) ) })
 	$(".submit-form").click(function() { interface.submitForm( $(this) ) } )
+	
+	$(".msg-input").keydown(function(e) {
+		
+		if (e.keyCode == 13) {
+			e.preventDefault();
+			$("#submit-msg-btn").trigger("click");
+		}	
+		
+	});
+	
+	$(".sub-input").keydown(function(e) {
+		
+		if (e.keyCode == 13) {
+			e.preventDefault();
+			$("#submit-email-btn").trigger("click");
+		}	
+		
+	});
 
 })
 
@@ -44,17 +62,41 @@ Interface.prototype.submitForm = function ( $this ) {
 	switch(id) {
 
 		case "submit-msg-btn":
+		
 			var email = $("#email").val();
 			var name = $("#name").val();
 			var msg = $("#msg").val();
-			if ( !interface.validate("email", email) ) $("#email").parent().addClass("error"); 
-			if ( !interface.validate("name", name) ) $("#name").parent().addClass("error"); 
-			if ( !interface.validate("msg", msg) ) $("#msg").parent().addClass("error"); 
+			
+			if ( !interface.validate("email", email) ) {
+				$("#email").parent().addClass("error"); 
+				return false;
+			} 
+			
+			if ( !interface.validate("name", name) ) {
+				$("#name").parent().addClass("error"); 
+				return false;
+			}
+			
+			if ( !interface.validate("msg", msg) ) {
+				$("#msg").parent().addClass("error"); 
+				return false;
+			}
+			
+			server.sendMsg( ["1", email, name, msg] );
+			
 			break;
 
 		case "submit-email-btn":
+		
 			var email = $("#email-subscribe").val();
-			if ( !interface.validate("email", email) ) $("#email-subscribe").parent().addClass("error"); 
+			
+			if ( !interface.validate("email", email) ) {
+				$("#email-subscribe").parent().addClass("error"); 
+				return false;
+			} 
+			
+			server.sendMsg( ["2", email] );
+			
 			break;
 
 		default:
@@ -110,3 +152,39 @@ Interface.prototype.openMenu = function() {
 }
 
 var interface = new Interface();
+
+var Server = function() {
+	
+}
+
+Server.prototype.sendMsg = function( data ) {
+	
+	console.log("sending :" + email + " - " + name +  " - " + msg);
+	
+	$.ajax({
+		url: '/assets/php/dgser.php',
+		type: 'POST',
+		dataType: 'JSON',
+		data: {
+			data: data
+		},
+		success: function(data) {
+			
+			if (data[0] == "1") {
+				console.log("Message has been sent");
+			} else if (data[1] == "2") {
+				console.log("Subscribed");
+			}
+			
+		},
+		error: function(data) {
+
+			console.log(data.responseText);
+			console.log("Error while communicating");
+		
+		}
+	})
+	
+}
+
+var server = new Server();
